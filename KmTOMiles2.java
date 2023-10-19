0b81e820445e1e73c237a8b5dadbf0b9d57f2f4e
Preston-Wong <you@example.com>
2023-10-19 09:09:14 -0400

    Made a better table

--- KmTOMiles2.java
+++ KmTOMiles2.java
@@ -1,22 +1,24 @@
-class Program1 extends ConsoleProgram {
+class KmTOMiles2 extends ConsoleProgram {
 
 /**
   * Description
   * A program that converts kilometers to miles from 10 to 100km, every 10km in a formatted table
-  * @author: E. Fabroa
+  * @author: Preston Wong
   */
 
   
   public void run() {
 
-     double dblMiles;
+    double dblMiles;
 
-
+    // header of the table 
+    System.out.printf("%8S %11S %n","Km", "Mi");
+    System.out.println("-------------------------");
+    
+    // prints out the table
     for (int intKm = 10; intKm <= 100; intKm += 10){
       dblMiles = intKm * 0.621371;
-      System.out.println(intKm + "km --> " + dblMiles + "mi");
-
-   
-    
+      System.out.printf("%7dkm %10.2fmi %n",intKm, dblMiles);
+    }
   }
 }
